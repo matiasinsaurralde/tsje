b187aa147fb429fa9460c55d9a5f3395995ea78f
Matias Insaurralde <matiasbaruchinsaurralde@gmail.com>
2014-02-07 01:17:14 -0300

    capturando la salida de curl, y falseando cabeceras de xhr, user-agent, etc.

--- tsje.rb
+++ tsje.rb
@@ -1,4 +1,12 @@
 require 'nokogiri'
 
+USER_AGENT = 'Mozilla Firefox'
+URL = 'http://tsje.gov.py/e2013/dinamics/app/consulta-al-padron.php'
+REFERER = 'http://tsje.gov.py/e2013/consulta-al-padron.html'
+
 module TSJE
+  def self.consulta( cedula )
+    raw_html = `curl -s -A '#{USER_AGENT}' -H 'X-Requested-With: XMLHttpRequest' -H 'Referer: #{REFERER}' #{URL} --data 'cedula=#{cedula}'`
+    print raw_html
+  end
 end
